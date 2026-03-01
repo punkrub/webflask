843f193ec351e35f890c6299132ea5ff6a86486d
Pun Suwannasang <punpunkapaipai@gmail.com>
2026-03-01 12:32:59 +0700

    update app

--- app.py
+++ app.py
@@ -18,3 +18,16 @@ def index():
     items = c.fetchall()
     conn.close()
     return render_template('index.html', items=items)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute('INSERT INTO menu (name, price) VALUES (?, ?)', (name, price))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('add.html')
